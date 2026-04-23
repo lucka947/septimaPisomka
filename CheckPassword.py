da2f3b3a57abd789a61d197201cb1e555909f43a
lucka947 <lmoricova@centrum.sk>
2026-04-23 08:08:31 +0200

    vvvv
    vvvv

--- CheckPassword.py
+++ CheckPassword.py
@@ -6,17 +6,23 @@ def check_password(password: str) -> bool:
     small_letter = False
     capital_letter = False
     number = False
+    lenght = False
 
-    for c in password:
-        if(c.isdigit()):
-            number = True
-        elif(c.islower()):
-            small_letter = True
-        elif(c.isupper()):
-            capital_letter = True
-    
-    return small_letter and capital_letter and number
+password = password.strip()
 
+if len(password) >= 8:
+    length = True
+
+
+for c in password:
+    if(c.isdigit()):
+        number = True
+    elif(c.islower()):
+        small_letter = True
+    elif(c.isupper()):
+        capital_letter = True
+
+return small_letter and capital_letter and number and length
 
 dir = os.path.dirname(__file__)                     # Nacita cestu k suboru
 with open(dir + "/passwords.txt", 'r') as file:     # Otvori subor s heslami
